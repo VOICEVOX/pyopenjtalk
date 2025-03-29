26e90f513bae948859f70d224c8156668b3691bd
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-30 08:49:27 +0900

    chore: from subprocess import runをやめる

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from looseversion import LooseVersion
 from glob import glob
 from itertools import chain
 from os.path import exists, join
-from subprocess import run
 
 import numpy as np
 import setuptools.command.build_py
@@ -186,7 +185,7 @@ if not exists(join(src_top, "mecab", "src", "config.h")):
 
     # NOTE: The wrapped OpenJTalk does not depend on HTS_Engine,
     # but since HTSEngine is included in CMake's dependencies, it refers to a dummy path.
-    r = run(["cmake", "..", "-DHTS_ENGINE_INCLUDE_DIR=.", "-DHTS_ENGINE_LIB=dummy"])
+    r = subprocess.run(["cmake", "..", "-DHTS_ENGINE_INCLUDE_DIR=.", "-DHTS_ENGINE_LIB=dummy"])
     r.check_returncode()
     os.chdir(cwd)
 
